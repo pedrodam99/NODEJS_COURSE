073d66c2c879c7d4e09ccf7fe9658e82e3926e0c
Pedro Dambrosio <pedrodam99@gmail.com>
2022-06-20 16:08:45 -0300

    Explorando o console

---
+++ 02_FUNDAMENTOS/07_explorando_console/index.js
@@ -0,0 +1,16 @@
+// imprimindo mais de um valor
+const x = 10;
+const y = "Texto";
+const z = [0, 10];
+console.log(x, y, z);
+
+//contagem de impressoes
+console.count("contagem: ");
+console.count("contagem: ");
+console.count("contagem: ");
+console.count("contagem: ");
+
+//limpar o console
+setTimeout(() => {
+  console.clear();
+}, 2000);
