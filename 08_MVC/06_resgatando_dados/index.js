92dca46fa254bd93bc19e10445c9ca4c868158f5
Pedro Dambrosio <pedrodam99@gmail.com>
2022-08-30 14:58:15 -0300

    Resgatando os dados

---
+++ 08_MVC/06_resgatando_dados/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const port = "3000";
+const app = express();
+
+const conn = require("./db/conn");
+
+const Task = require("./models/Task");
+
+const taskRoutes = require("./routes/tasksRoutes");
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use("/tasks", taskRoutes);
+
+conn
+  .sync()
+  .then(() => app.listen(port))
+  .catch((err) => console.log(err));
